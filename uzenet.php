799b2d6e37ce6ceb19762273d5473c4624063a71
Peti1021 <peti1021@gmail.com>
2015-02-11 10:13:27 +0100

    megoldas feltoltes

---
+++ uzenet.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Üzenet érkezett</h1>
+       Név: <?php echo $_POST["nev"]; ?><br>
+       Telefon: <?php echo $_POST["telefon"]; ?><br>
+       E-mail: <?php echo $_POST["e-mail"]; ?><br>
+       Üzenet: <?php echo $_POST["uzenet"]; ?><br>
+       <br>
+       <a href="index.php">Üzenet</a>
+
+
+
+
+    </body>
+</html>
+
+
